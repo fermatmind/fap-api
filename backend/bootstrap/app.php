025d1039e10e49143faa310e3faaeb0bc3671fd3
fermatmind <17551046983@163.com>
2026-07-05 16:54:09 +0800

    IQ-METHOD-PAGES-ZH-CN-CMS-POST-PUBLISH-READBACK-01: add post-publish readback

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -33,6 +33,7 @@ return Application::configure(basePath: dirname(__DIR__))
         \App\Console\Commands\ArticleImportIqMethodPagesDraft::class,
         \App\Console\Commands\ArticleIqMethodPagesPublishGate::class,
         \App\Console\Commands\ArticleIqMethodPagesPublish::class,
+        \App\Console\Commands\ArticleIqMethodPagesPostPublishReadback::class,
         \App\Console\Commands\ArticleIqMethodPagesReadback::class,
         \App\Console\Commands\ArticleIqMethodPagesReviewApproval::class,
         \App\Console\Commands\PersonalityEnneagramCmsPromote::class,
