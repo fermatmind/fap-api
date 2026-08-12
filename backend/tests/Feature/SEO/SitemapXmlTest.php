f10f44588b1e8fd33dd6aa08be4eb93ecbe3a389
fermatmind <17551046983@163.com>
2026-08-13 00:00:43 +0800

    test(career): preserve held permit sitemap expectation

--- backend/tests/Feature/SEO/SitemapXmlTest.php
+++ backend/tests/Feature/SEO/SitemapXmlTest.php
@@ -515,8 +515,8 @@ class SitemapXmlTest extends TestCase
         $this->assertStringNotContainsString('<loc>https://staging.fermatmind.com/en/articles/article-draft</loc>', $body);
         $this->assertStringNotContainsString('<loc>https://staging.fermatmind.com/en/career/jobs</loc>', $body);
         $this->assertStringNotContainsString('<loc>https://staging.fermatmind.com/zh/career/jobs</loc>', $body);
-        $this->assertStringContainsString('<loc>https://staging.fermatmind.com/en/career/jobs/agricultural-inspectors</loc>', $body);
-        $this->assertStringContainsString('<loc>https://staging.fermatmind.com/zh/career/jobs/agricultural-inspectors</loc>', $body);
+        $this->assertStringNotContainsString('<loc>https://staging.fermatmind.com/en/career/jobs/agricultural-inspectors</loc>', $body);
+        $this->assertStringNotContainsString('<loc>https://staging.fermatmind.com/zh/career/jobs/agricultural-inspectors</loc>', $body);
         $this->assertStringNotContainsString('<loc>https://staging.fermatmind.com/en/career/jobs/product-manager</loc>', $body);
         $this->assertStringNotContainsString('<loc>https://staging.fermatmind.com/zh/career/jobs/product-manager</loc>', $body);
         $this->assertStringNotContainsString('<loc>https://staging.fermatmind.com/en/career/jobs/private-role</loc>', $body);
